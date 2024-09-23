e7f99de44da7fb4341c3e0439408489053822b37
Paulo.R.F.Pizza <117474750+paulo626@users.noreply.github.com>
2024-09-23 13:20:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function HistoriaJogo(){
+
+}
+function posicionar() {
+    const filho = document.getElementById('Personagem');
+
+    const x = 50; 
+    const y = 80;  
+    filho.style.left = x + 'px';
+    filho.style.top = y + 'px';
+}
