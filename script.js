6d1a0a1876c600863ac8e8907b08a86d16a29c4e
Paulo.R.F.Pizza <117474750+paulo626@users.noreply.github.com>
2024-09-25 13:37:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function HistoriaJogo(){
 
+function AlertaAi(){
+    alert('foi')
 }
-function posicionar() {
-    const filho = document.getElementById('Personagem');
+function MovimentarPersonagem(vez){
+    let listaDePossição = {1:[30,50],2:[170,50],3:[665,888]}
 
-    const x = 50; 
-    const y = 80;  
-    filho.style.left = x + 'px';
-    filho.style.top = y + 'px';
+    Personagem.style.left = listaDePossição[vez][0] + 'px';
+    Personagem.style.top = listaDePossição[vez][1] + 'px';
 }
+async function Jogar(){
+    const botaoAvancar = document.getElementById("Botaoavancar")
+    botaoAvancar.addEventListener('click',() => MovimentarPersonagem(1));
+}
+Jogar()
+
